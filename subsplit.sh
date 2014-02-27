14d5a510618bc888bc87765823522d05dfa9e698
Stéphane Goetz <onigoetz@onigoetz.ch>
2014-02-28 00:39:54 +0100

    Added subsplitter

---
+++ subsplit.sh
@@ -0,0 +1,4 @@
+#git subsplit available here : https://github.com/dflydev/git-subsplit
+git subsplit init git@github.com:RocketPropelledTortoise/Core.git
+git subsplit publish src/Translation:git@github.com:RocketPropelledTortoise/Translation.git
+rm -rf .subsplit/
