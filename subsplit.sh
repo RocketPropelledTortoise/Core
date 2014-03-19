46cfdaa2b0976a89ca308583de5514c2344ac18b
Stéphane Goetz <onigoetz@onigoetz.ch>
2014-03-19 22:49:08 +0100

    Update Core informations

--- subsplit.sh
+++ subsplit.sh
@@ -1,4 +1,6 @@
 #git subsplit available here : https://github.com/dflydev/git-subsplit
 git subsplit init git@github.com:RocketPropelledTortoise/Core.git
+git subsplit publish src/Taxonomy:git@github.com:RocketPropelledTortoise/Taxonomy.git
 git subsplit publish src/Translation:git@github.com:RocketPropelledTortoise/Translation.git
+git subsplit publish src/Utilities:git@github.com:RocketPropelledTortoise/Utilities.git
 rm -rf .subsplit/
